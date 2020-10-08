21a92189b602a68cb2b7ee060c43eeae09a17039
guidotripodi <guido.tripodi@hotmail.com>
2020-10-08 16:07:00 -0300

    subiendo lista

--- lib/LocationClass.dart
+++ lib/LocationClass.dart
@@ -12,9 +12,9 @@ class Locacion {
 
 final locaciones = [
   new Locacion(name:'TU VIEJA',capacidad: 1,infectados:1),
- // new Locacion(name:'TU HERMANA',capacidad: 1,infectados:1),
- // new Locacion(name:'GOL SACANDO DEL MEDIO',capacidad: 1,infectados:1),
- // new Locacion(name:'TU VIEJA 2',capacidad: 1,infectados:1),
- // new Locacion(name:'TU HERMANA 2',capacidad: 1,infectados:1),
-  //new Locacion(name:'TU VIEJA 3',capacidad: 1,infectados:1),
+  new Locacion(name:'TU HERMANA',capacidad: 1,infectados:1),
+  new Locacion(name:'GOL SACANDO DEL MEDIO',capacidad: 1,infectados:1),
+  new Locacion(name:'TU VIEJA 2',capacidad: 1,infectados:1),
+  new Locacion(name:'TU HERMANA 2',capacidad: 1,infectados:1),
+  new Locacion(name:'TU VIEJA 3',capacidad: 1,infectados:1),
 ];
--- lib/verLocacionScreen.dart
+++ lib/verLocacionScreen.dart
@@ -5,64 +5,20 @@ import 'StyleUtils.dart';
 import 'UserScreen.dart';
 import 'LocationClass.dart';
 
-class VerLocationsScreen extends StatefulWidget {
-  @override
-  _VerLocationsScreenState createState() => _VerLocationsScreenState();
-}
-
-class _VerLocationsScreenState extends State<VerLocationsScreen> {
+class VerLocationsScreen extends StatelessWidget {
   // This widget is the root of your application.
   @override
-  Widget _buildList(BuildContext context) {
-    return new ListView(
-              children: locaciones.map(_buildItem).toList(),
-      );
-  }
-
-
-  Widget _buildBackBtn() {
-    return Container(
-      padding: EdgeInsets.symmetric(vertical: 25.0),
-      width: double.infinity,
-      child: RaisedButton(
-        elevation: 5.0,
-        onPressed: () {
-          Navigator.pop(context);
-        },
-        padding: EdgeInsets.all(15.0),
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(30.0),
-        ),
-        color: Colors.white,
-        child: Text(
-          'VOLVER',
-          style: TextStyle(
-            color: Color(0xFF527DAA),
-            letterSpacing: 1.5,
-            fontSize: 18.0,
-            fontWeight: FontWeight.bold,
-            fontFamily: 'OpenSans',
-          ),
-        ),
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Países',
+      theme: new ThemeData(
+        primarySwatch: Colors.blue,
       ),
-    );
-  }
-
-    Widget build(BuildContext context) {
-    return Scaffold(
-        body: BackgroundFrame(
-            child: Form(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Text('Locaciones:', style: subtitleTextStyle),
-                    SizedBox(height: 30.0),
-                    _buildList(context),
-                    _buildBackBtn(),
-                  ],
-                )
-            )
-        )
+      home: Scaffold(
+          body: new ListView(
+              children: locaciones.map(_buildItem).toList(),
+          )
+      )
     );
   }
 }
@@ -77,4 +33,3 @@ Widget _buildItem(Locacion locacion) {
       },
   );
 }
-
